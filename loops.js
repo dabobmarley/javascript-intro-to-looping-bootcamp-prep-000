4ca567aefea7b727661523a329d741ef63e63b22
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-23 08:51:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -27,10 +27,13 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
  
-do {
-  console.log('doo-bee-doo-bee-doo')
-} while (maybeTrue());  
 }
- function doWhileLoop(){
+ function doWhileLoop(array){
+   do {
+     array.pop();
+   }
+   while (array.length > 0 && maybeTrue());
+   return array;
+   
    
  }
